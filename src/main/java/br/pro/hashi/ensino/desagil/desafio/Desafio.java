0d5173cb05ceb9e61707e2c2d1223862d991d562
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-02-21 07:47:01 -0300

    Add target

--- src/main/java/br/pro/hashi/ensino/desagil/desafio/Desafio.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/Desafio.java
@@ -2,6 +2,11 @@ package br.pro.hashi.ensino.desagil.desafio;
 
 public class Desafio {
     public static void main(String[] args) {
+        Target target = new Target();
+        target.row = 4;
+        target.col = 14;
+        System.out.println("Target: " + target.row + ", " + target.col);
+
         Player humanPlayer = new Player();
         humanPlayer.row = 0;
         humanPlayer.col = 0;
