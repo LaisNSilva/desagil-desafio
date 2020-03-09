9c707e396bd0b7cdcfd871ad72c5adf434272730
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-03-09 07:02:09 -0300

    Add methods to player subclasses

--- src/main/java/br/pro/hashi/ensino/desagil/desafio/model/CpuPlayer.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/model/CpuPlayer.java
@@ -4,4 +4,8 @@ public class CpuPlayer extends Player {
     public CpuPlayer(int row, int col, Board board) {
         super(row, col, board);
     }
+
+    public void move() {
+        System.out.println("move");
+    }
 }
--- src/main/java/br/pro/hashi/ensino/desagil/desafio/model/HumanPlayer.java
+++ src/main/java/br/pro/hashi/ensino/desagil/desafio/model/HumanPlayer.java
@@ -4,4 +4,20 @@ public class HumanPlayer extends Player {
     public HumanPlayer(int row, int col, Board board) {
         super(row, col, board);
     }
+
+    public void moveUp() {
+        System.out.println("moveUp");
+    }
+
+    public void moveRight() {
+        System.out.println("moveRight");
+    }
+
+    public void moveDown() {
+        System.out.println("moveDown");
+    }
+
+    public void moveLeft() {
+        System.out.println("moveLeft");
+    }
 }
